044615b8b422de328b5cc4e661e1ff5a27b56bdf
邓婷 <dengting.meituan.com>
2017-05-16 00:04:55 +0800

    decodeCount first commit

---
+++ src/decodeCount.js
@@ -0,0 +1,30 @@
+function decodeCount(input) {
+    if (!input) return 0;
+    input = input.toString();
+    if (!/^\d+$/.test(input)) return 0;
+    var len = input.length;
+    var i = 0;
+    if (len === 1) return 1;
+    for (; i < len; i++) {
+        var cur  = input[i];
+        var next = input[i+1];
+        if ((+cur === 2) && (+next <= 6) || (+cur === 1)) {
+            return i+2 < len ? decodeCount(input.substring(i+1)) + decodeCount(input.substring(i+2)) : 2;
+        }
+    }
+    return 1;
+}
+
+module.exports = decodeCount;
+
+/**题目:
+有一个消息包含A-Z通过以下规则编码:
+ 
+'A' -> 1
+'B' -> 2
+...
+'Z' - > 26
+ 
+现在给你一个加密的信息,问有几种解密的方式
+// eg:'12'可以解析为'AB' 或 'L'
+ */
---
+++ test/decodeCount.test.js
@@ -0,0 +1,46 @@
+/**
+ * Created by LuckyDottie on 2017/5/14.
+ */
+var assert = require('assert');
+var decodeCount = require('../src/decodeCount');
+describe('decodeCount test cases', function(){
+    it('should return 0', function(){
+        assert.equal(0, decodeCount('aasda87897'));
+    });
+    it('should return 0', function(){
+        assert.equal(0, decodeCount('9090cc5'));
+    });
+    it('should return 0', function(){
+        assert.equal(0, decodeCount());
+    });
+    it('should return 1', function(){
+        assert.equal(1, decodeCount(1));
+    });
+    it('should return 1', function(){
+        assert.equal(1, decodeCount('1'));
+    });
+    it('should return 1', function(){
+        assert.equal(1, decodeCount('2'));
+    });
+    it('should return 2', function(){
+        assert.equal(2, decodeCount('12'));
+    });
+    it('should return 2', function(){
+        assert.equal(2, decodeCount('18'));
+    });
+    it('should return 2', function(){
+        assert.equal(2, decodeCount('26'));
+    });
+    it('should return 1', function(){
+        assert.equal(1, decodeCount('27'));
+    });
+    it('should return 1', function(){
+        assert.equal(1, decodeCount('333'));
+    });
+    it('should return 3', function(){
+        assert.equal(3, decodeCount('1247'));
+    });
+    it('should return 2', function(){
+        assert.equal(2, decodeCount('1277'));
+    });
+});
